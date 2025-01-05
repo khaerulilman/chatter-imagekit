9b1adbb037a5172e2cf8291990073206c945568f
Mochammad khaerul ilman <110533356+khaerulilman@users.noreply.github.com>
2025-01-05 15:26:31 +0700

    Update getPosts.js

--- controllers/get/getPosts.js
+++ controllers/get/getPosts.js
@@ -2,12 +2,18 @@ import db from "../../config/db.js";
 
 const getPosts = async (req, res) => {
   try {
+    // Cek token dan autentikasi pengguna
+    const token = req.headers.authorization?.split(' ')[1];
+    if (!token) {
+      return res.status(401).json({ message: 'Unauthorized' });
+    }
+
     // Ambil semua data post dari tabel posts
     const posts = await db`SELECT * FROM posts`;
 
     // Kirimkan respons dengan data semua posts
     res.status(200).json({
-      message: "Users retrieved successfully.",
+      message: "Posts retrieved successfully.",
       data: posts,
     });
   } catch (error) {
